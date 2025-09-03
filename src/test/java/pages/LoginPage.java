dbb3c70228161d982aa573cd93ebbfcdb4de53fd
Aji <aj@gmail.com>
2025-09-03 15:10:24 +0200

    changed homepage,Loginpage,

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,6 +2,8 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage {
 
@@ -11,11 +13,28 @@ public class LoginPage {
     private By usernameField = By.name("email");
     private By passwordField = By.name("password");
     private By loginButton = By.cssSelector("button.btn.btn-primary");
+    private By emailerrorMessage = By.xpath("//div[text()='Invalid email']");
+    private By passwordRequiredMessage = By.xpath("//div[text()='Password is required']");
+    private By invalidLoginmessage= By.xpath("//div[text()='Invalid email or password. Please check your credentials and try again.']");
 
 
     // Constructor
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).clear();
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).clear();
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
     }
 
     public void login(String username, String password) {
@@ -26,5 +45,17 @@ public class LoginPage {
         driver.findElement(passwordField).sendKeys(password);
 
         driver.findElement(loginButton).click();
+    }
+
+    public String getInvalidEmailMessage() {
+        return driver.findElement(emailerrorMessage).getText();
+    }
+
+    public String passwordRequiredMessage() {
+        return driver.findElement(passwordRequiredMessage).getText();
+    }
+
+    public String getInvalidLoginMessage(){
+        return  driver.findElement(invalidLoginmessage).getText();
     }
 }
