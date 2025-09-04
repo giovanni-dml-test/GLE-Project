6ce5165fbe944fb9d3ebe0a63dbd27cfd8651210
Aji <aj@gmail.com>
2025-09-04 20:50:15 +0200

    created propertydetailpage, EditAdvertPage changed featurefile ,stepdef,homepage,

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -7,6 +7,7 @@ import utils.Driver;
 
 public class HomePage {
     public HomePage()
+
     {PageFactory.initElements(Driver.getDriver(), this);}
 
 
@@ -86,6 +87,9 @@ public class HomePage {
     @FindBy(xpath = "//button[text()='SHOP']")
     public WebElement shopTab;
 
+ public void clickLogin(){
+     login.click();
+ }
 
 
 
